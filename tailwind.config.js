b232402b02cb7f7ad8a2cbe97f7b7415304b9c18
rezwanhossen <rezwanhossen625@gmail.com>
2024-09-17 01:00:13 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: { themes: ["light"] },
+};
