d850e526ec0859969022170913030f5721596ce6
rezwanhossen <rezwanhossen625@gmail.com>
2024-09-17 23:40:12 +0600

    add home and footer

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,8 @@ import { createRoot } from "react-dom/client";
 import * as ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./Route/Route.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
