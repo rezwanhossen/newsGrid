823593b0421c49670e89daf10080bd20073c86ab
rezwanhossen <rezwanhossen625@gmail.com>
2024-09-18 10:44:20 +0600

    setup Fairbase

---
+++ src/Components/Fairbase/firbase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSjwgHhXmeclKhutCS_zBCApGaZWOf_mE",
+  authDomain: "newsgrid-95245.firebaseapp.com",
+  projectId: "newsgrid-95245",
+  storageBucket: "newsgrid-95245.appspot.com",
+  messagingSenderId: "395499346338",
+  appId: "1:395499346338:web:bef182f58ab5592d39bafb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
