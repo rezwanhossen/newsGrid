0dcb716bf260597f015de524cab628ef6886553d
Naimul02 <naimulislum39@gmail.com>
2024-09-18 13:01:22 -0700

    banner section added

---
+++ src/Components/Home/Banner/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import CardLayout from "../CardLayout/CardLayout";
+import Slider from "../Slider/Slider";
+
+
+
+const Banner = () => {
+    return (
+        <>
+                <div className="flex gap-4 flex-col lg:flex-row">
+                        {/* carousel */}
+                        <div className="w-full lg:w-[55%]">
+                                <Slider></Slider>
+                        </div>
+                        {/* grid layout */}
+                        <div className="w-full lg:w-[45%]">
+                                <CardLayout></CardLayout>
+                        </div>
+                </div>
+        </>
+    );
+};
+
+export default Banner;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,9 @@
 import FeaturedSection from "./FeaturedSection/FeaturedSection";
-
+import Banner from './Banner/Banner/Banner'
 const Home = () => {
   return (
     <div>
+      <Banner/>
       <FeaturedSection></FeaturedSection>
     </div>
   );
