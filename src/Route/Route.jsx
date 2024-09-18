41b3f2fdce5bd1787ff922deded4761d823ca5b4
rezwanhossen <rezwanhossen625@gmail.com>
2024-09-19 01:18:43 +0600

    add error page

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -3,11 +3,13 @@ import Root from "../Components/Layout/Root";
 import Home from "../Components/Home/Home";
 import SignUp from "../Components/LoginRegister/SingUp/SignUp";
 import Login from "../Components/LoginRegister/Login/Login";
+import Erroe from "../Components/Pages/Error/Erroe";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <Erroe />,
     children: [
       {
         path: "/",
