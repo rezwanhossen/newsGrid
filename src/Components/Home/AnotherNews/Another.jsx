ca38955f24ed1a562710343693b440d1f6691bea
Naimul02 <naimulislum39@gmail.com>
2024-09-26 11:13:18 -0700

    another comment

---
+++ src/Components/Home/AnotherNews/Another.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import LeftSide from './LeftSide';
+import RightSide from './RightSide';
+
+const Another = () => {
+    return (
+        
+        <div className="max-w-7xl mx-auto">
+            {/* another page */}
+            <div className="flex gap-5">
+                <div  className="w-[60%]">
+                <LeftSide></LeftSide>
+                </div>
+                <div className="w-[40%]">
+                <RightSide></RightSide>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Another;
