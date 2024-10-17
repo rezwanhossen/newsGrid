35749f531c67c8d5ee21a37b22f1c62506f77eb3
rafietrana <bdmail674@gmail.com>
2024-10-17 13:13:31 +0600

    completed clint side code

--- src/Components/PersonalizedNews/PersonalizedNews.jsx
+++ src/Components/PersonalizedNews/PersonalizedNews.jsx
@@ -8,7 +8,7 @@ const PersonalizedNews = () => {
   const [objectDataForStoreInMongodb, setObjDataForStoreInMongodb] =
     useState(null);
   console.log("alhamdulillah category is", selectedCategory);
-
+// alhamdulillah
   const { user } = useAuth();
   const userEmail = user.email;
 
